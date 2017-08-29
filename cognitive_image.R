925235579727b5e8abf9135fc5e8a7056adb94f7
Paige Bailey <pabailey@microsoft.com>
2017-08-28 22:35:06 -0500

    Update cognitive_image.R

--- cognitive_image.R
+++ cognitive_image.R
@@ -3,11 +3,15 @@
 # =============================================
 
 # =============================================
-# Request Parameters
+# Required Packages
 # =============================================
-#
-#
-#
+
+library(httr) # bae
+library(jsonlite)
+
+# =============================================
+# Grungy, so grungy
+# =============================================
 
 getImage(key, q, n) {
   request_body <- data.frame(id = "1", stringsAsFactors = FALSE)
@@ -15,4 +19,10 @@ getImage(key, q, n) {
   
   result <- POST("https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=" + as.string(q) + "&count=" + as.string(n),
                  body = request_body_json,
-                 add_headers(.headers =
+                 add_headers(.headers = c("Content-Type" = "application/json",
+                                          "Ocp-Apim-Subscription-Key" = key)))
+  
+  return(content(result))
+  }
+
+
