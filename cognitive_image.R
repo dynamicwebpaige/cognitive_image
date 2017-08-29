d1d3faec9c1440b254cb31208c7e88310d21384c
Paige Bailey <pabailey@microsoft.com>
2017-08-28 22:32:53 -0500

    Update cognitive_image.R

--- cognitive_image.R
+++ cognitive_image.R
@@ -8,3 +8,11 @@
 #
 #
 #
+
+getImage(key, q, n) {
+  request_body <- data.frame(id = "1", stringsAsFactors = FALSE)
+  request_body_json <- toJSON(list(documents = request_body), auto_unbox = TRUE)
+  
+  result <- POST("https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=" + as.string(q) + "&count=" + as.string(n),
+                 body = request_body_json,
+                 add_headers(.headers =
